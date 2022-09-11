3c6744c471d58c0d8f0225bd71708580a1b85d3c
punisher880 <bhambleaakash21@gmail.com>
2022-09-11 14:22:42 +0530

    Npm i ejs-edit and make styling in ejs with bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -14,9 +14,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() *20)+10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image:'https://unsplash.com/collections/483251/in-the-woods',
+            description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quidem, officia nesciunt tenetur numquam dolor soluta. Saepe atque inventore nobis placeat omnis quas, itaque in labore velit, necessitatibus excepturi autem.",
+            price
         })
         await camp.save();
     }
